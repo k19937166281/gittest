26538c8e0dc3c5c5324c51cfeacba80844527fe6
wanggaowei <k19937166281@126.com>
2019-07-11 23:44:31 +0800

    wgw commit 添加一个字段

--- src/main/java/com/wgw/test02/role.java
+++ src/main/java/com/wgw/test02/role.java
@@ -10,10 +10,11 @@ public class role {
     private String ii;
     //王高威
     private String wgw;
-
+    private String 先生神人也;
     //和平安
     private String hpa;
     private String ght;
     private String zwf;
     private String xwy;
+
 }
